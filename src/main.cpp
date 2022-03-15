88d3e4e73cdfea1d13d4f026ec7824a641e139cf
krr <benjamin.robles.a@mail.pucv.cl>
2022-03-15 18:18:54 -0300

    mv to ft2mm

--- src/main.cpp
+++ src/main.cpp
@@ -2,57 +2,7 @@
 
 #include <GLFW/glfw3.h>
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
-
-namespace FreeType2 {
-    class Library 
-    {
-    public:
-        FT_Library base;
-        Library();
-    };
-    
-    class Face 
-    {
-        public:
-            FT_Face base;
-
-            Face(Library     library, 
-                std::string  filePath,
-                int          faceIndex);
-
-            void setSize(int pxW, int pxH);
-    };
-    
-};
-
-namespace FreeType2 
-{
-    Library::Library()
-    {
-        if (FT_Init_FreeType(&base))
-            std::cout << "ERROR: couldn\'t init ft2\n";
-    }
-
-    Face::Face( Library     library, 
-                std::string filePath,
-                int     faceIndex)
-    {
-        if (FT_New_Face(library.base, filePath.c_str(),
-                    faceIndex, &base))
-            std::cout << "ERROR: couldn\'t load font\n";
-    }
-
-    void Face::setSize(int pxW, int pxH)
-    {
-        if (FT_Set_Pixel_Sizes(base, pxW, pxH))
-            std::cout << "ERROR: couldn\'t set font size\n";
-
-    }
-}
-            
-
+#include "ft2mm.hpp"
 
 int main() {
     FreeType2::Library ft2lib;
