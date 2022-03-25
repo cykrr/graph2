d57d8bee345f9ff98a73508867ebb1d82e31dad6
Your Name <you@example.com>
2022-03-25 18:16:58 -0300

    add glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,35 @@
 #include <iostream>
 
-#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 #include "ft2mm.hpp"
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+#include "vkfw/vkfw.hpp"
+
+namespace GUI {
+    bool init();
+}
+namespace GUI {
+    bool init()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        return EXIT_SUCCESS;
+#ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            std::cout << "Failed to initialize GLAD" << std::endl;
+            return -1;
+        }
+    }
+}
 
 int main() {
-    FreeType2::Library ft2lib;
-    FreeType2::Face face(ft2lib, "fonts/Cantarell.otf", 0);
-    face.setSize(0, 48);
-    for( unsigned char c = 0; c < 128; c++)
-    {
-        face.loadChar(c, FT_LOAD_RENDER);
-        unsigned int texture;
-        glGenTextures(1, &texture);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        glTexImage2D(
-                GL_TEXTURE_2D,
-                0, 
-                GL_RED,
-                face.base->glyph->bitmap.width,
-                face.base->glyph->bitmap.rows,
-                0, 
-                GL_RED,
-                GL_UNSIGNED_BYTE,
-                face.base->glyph->bitmap.buffer
-                );
-
-        glTexParameteri(
-                GL_TEXTURE_2D, 
-                GL_TEXTURE_WRAP_S, 
-                GL_CLAMP_TO_EDGE);
-
-        glTexParameteri(GL_TEXTURE_2D,
-                GL_TEXTURE_WRAP_T,
-                GL_CLAMP_TO_EDGE);
-
-        glTexParameteri(GL_TEXTURE_2D,
-                GL_TEXTURE_MIN_FILTER,
-                GL_LINEAR);
-
-        glTexParameteri(GL_TEXTURE_2D,
-                GL_TEXTURE_MAG_FILTER,
-                GL_LINEAR);
-    }
-
-
+    GUI::init();
 }
