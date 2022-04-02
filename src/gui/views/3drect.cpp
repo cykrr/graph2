d9cbc95a1ebca7437b1555d8546329f890c6b452
Your Name <you@example.com>
2022-04-02 10:55:59 -0300

    change indices to a full blown rect

--- src/gui/views/3drect.cpp
+++ src/gui/views/3drect.cpp
@@ -7,29 +7,32 @@ Rect3d::Rect3d() {
         glm::vec3(1.f, 1.f, 0.f)
     };
     this->vertices = 4;
-    this->indices = 3;
+    this->indices = 6;
+    this->dim = 3;
 }
 
 void Rect3d::draw() {
     float vertexArr[] = {
-    -0.5f, -0.5f, -0.5f,  
-     0.5f, -0.5f, -0.5f,  
-     0.5f,  0.5f, -0.5f,  
+        0.f, 0.f, 0.f,
+        1.f, 0.f, 0.f,
+        0.f, 1.f, 0.f,
+        1.f, 1.f, 0.f
+        
     };
     unsigned int indicesArr[] = {
-        0, 1, 2
+        0, 1, 3,
+        0, 3, 2
     };
     
     glBufferSubData(GL_ARRAY_BUFFER, 0,
-            this->vertices*sizeof(float),
+            this->vertices*sizeof(float) * this->dim,
             vertexArr);
 
     glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0,
             this->indices*sizeof(unsigned int),
             indicesArr);
 
-//    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 }
 
